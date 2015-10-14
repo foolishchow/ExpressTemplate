95d61f4c9f128e28b242d221f3003fcf7394af2e
foolishchow <foolishchow@github.com>
2015-10-14 20:25:55 +0800

    updating

--- test/testDb.js
+++ test/testDb.js
@@ -5,18 +5,25 @@ var baseDao = require('../main/dao/baseDao/baseDao.js');
 
 var sql = "select * from td_m_resource where id =?";
 var array = ['1'];
-var array1 = ['2'];
-// baseDao(sql,array,function(result){
-// 	console.info("000");
-// 	console.info(result);
-// });
+var array1 = ['4','4','4'];
+var array2 = ['4','5','5'];
+var sql1 = "insert into td_m_resource(id,name,token)  values(?,?,?)";
 
+try{
 
-var bd = baseDao.base();
-	bd.addQuery(sql,array,function(result){
-		console.info("000");
-		console.info(result);
-	}).addQuery(sql,array1,function(result){
-		console.info("001");
-		console.info(result);
-	}).excute();
+	var bd = baseDao.base(true);
+		bd.addQuery(sql,array,function(result){
+			console.info("000");
+			console.info(result);
+		}).addQuery(sql1,array1,function(result){
+			console.info("001");
+			console.info(result);
+		}).addQuery(sql1,array2,function(result){
+			console.info("002");
+			console.info(result);
+		}).excute();
+
+}catch(e){
+	logger.info("---------------catch");
+	logger.info(e);
+}
